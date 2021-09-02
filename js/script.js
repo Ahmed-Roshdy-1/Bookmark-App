81b1c89e19c242724c10eec20bac8cf4c9df5e49
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-09-02 21:47:29 +0200

    Form validation

--- js/script.js
+++ js/script.js
@@ -13,7 +13,41 @@ let showModal = () => {
 };
 // Close Modal
 let closeModal = () => modal.classList.remove("show-modal");
-//Events Listeners
+
+// Modal Events Listeners
 modalShow.addEventListener("click", showModal);
 modalClose.addEventListener("click", closeModal);
-window.addEventListener("click", (e) => (e.target === modal ? closeModal() : false)); // close when click outside the modal
+// close when click outside the modal
+window.addEventListener("click", (e) => (e.target === modal ? closeModal() : false));
+
+// Validate Form
+let Validate = (nameValue, urlValue) => {
+  const expression =
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+  if (!nameValue || !urlValue) {
+    alert("please submit values for both fields.");
+    return false;
+  } else {
+    alert("please provide a valid web address");
+    return false;
+  }
+  // Valid
+  return true;
+};
+// Handle Data from form
+let storeBookmark = (e) => {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if (!urlValue.includes("http://", "https://")) {
+    urlValue = `https://${urlValue}`;
+  }
+  console.log(nameValue, urlValue);
+  if (!Validate(nameValue, urlValue)) {
+    return false;
+  }
+};
+
+// Event Listener
+bookmarkForm.addEventListener("submit", storeBookmark);
